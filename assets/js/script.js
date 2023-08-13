d623ce36a8d15e6a8b22b0eee96cbc8de31d20e7
arceojoshua94 <arceojoshua94@outlook.com>
2023-08-13 07:21:41 -0400

    troubleshooting javascript code, testing weather api application, added detailed notes to javascript code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,100 @@
-var apiKey="786f43eb470747904b392cc38a0feff7"
-var titleEl=document.getElementById("title")
-var tempEl=document.getElementById("temp")
-var windEl=document.getElementById("wind")
-var humidityEl=document.getElementById("humidity")
-var searchBtn=document.getElementById("search-btn")
-var cityInput=document.getElementById("city-input")
-var fivedayForcastEl= document.getElementById("fiveday-forcast")
+// Declare the API key for OpenWeatherMap
+const apiKey = "786f43eb470747904b392cc38a0feff7";
 
+// Get references to various HTML elements
+const searchBtn = document.getElementById("search-btn");
+const cityInput = document.getElementById("city-input");
+const searchHistory = document.getElementById("search-history");
+const currentWeatherRow = document.getElementById("current-weather");
+const forecastRow = document.getElementById("forecast");
 
-function searchCity(){
-    var cityName=cityInput.value
+// Attach the searchCity function to the click event of the search button
+searchBtn.addEventListener("click", searchCity);
 
-    displayWeather(cityName)
+// Function to handle the city search
+function searchCity() {
+  const cityName = cityInput.value;
+  if (cityName) {
+    // Call the displayWeather function and clear the input field
+    displayWeather(cityName);
+    cityInput.value = "";
+  }
 }
 
-function displayWeather(cityName){
-    var url="https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+ apiKey+"&units=imperial"
+// Function to display weather data
+function displayWeather(cityName) {
+  // Construct URLs for current weather and forecast API calls
+  const currentUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
+  const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=imperial`;
 
-    fetch(url)
-    .then(function(response){
-        return response.json()
+  // Fetch current weather data
+  fetch(currentUrl)
+    .then((response) => response.json())
+    .then((currentData) => {
+      // Call the displayCurrentWeather function and update search history
+      displayCurrentWeather(currentData);
+      updateSearchHistory(cityName);
     })
-    .then(function(currentData){
-        console.log(currentData)
-        titleEl.innerHTML=currentData.name + dayjs.unix(currentData.dt).format(" (MM/DD/YYYY)")+ "<img src='https://openweathermap.org/img/wn/"+ currentData.weather[0].icon+"@2x.png'>"
+    .catch((error) => console.error("Current Weather Error:", error));
+
+  // Fetch forecast data
+  fetch(forecastUrl)
+    .then((response) => response.json())
+    .then((forecastData) => {
+      // Call the displayForecast function
+      displayForecast(forecastData);
     })
-
-
-    var forecastUrl="https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid="+apiKey+"&units=imperial"
-
-    fetch(forecastUrl)
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(forecastData){
-        console.log(forecastData)
-        //grab every 12pm for each day for 5 days
-        var forecastArr=forecastData.list
-  
-        for (let i = 3,j=1; i < forecastArr.length; i=i+8,j++) {
-             console.log(forecastArr[i])
-               var cardTitle=document.getElementById("card-title"+j)
-               console.log("card-title"+j)
-               cardTitle.textContent=dayjs.unix(forecastArr[i].dt).format(" (MM/DD/YYYY)")
-               var temp=document.getElementById("temp"+j)
-
-               temp.textContent=forecastArr[i].main.temp
-        }
-
-    })
+    .catch((error) => console.error("Forecast Error:", error));
 }
 
+// Function to display current weather data
+function displayCurrentWeather(currentData) {
+  // Construct HTML to display current weather
+  const currentWeatherHTML = `
+    <div class="col-sm-12">
+      <h2>${currentData.name} (${dayjs.unix(currentData.dt).format("MM/DD/YYYY")}) <img src="https://openweathermap.org/img/wn/${currentData.weather[0].icon}@2x.png" alt="${currentData.weather[0].description}"></h2>
+      <p>Temperature: ${currentData.main.temp} °F</p>
+      <p>Humidity: ${currentData.main.humidity}%</p>
+      <p>Wind Speed: ${currentData.wind.speed} MPH</p>
+    </div>
+  `;
 
-searchBtn.addEventListener("click", searchCity)
+  // Update the current weather row with the generated HTML
+  currentWeatherRow.innerHTML = currentWeatherHTML;
+}
+
+// Function to display forecast data
+function displayForecast(forecastData) {
+  // Filter forecast data to only include entries at 12:00 PM (every 8th entry)
+  const forecastArr = forecastData.list.filter((_, index) => index % 8 === 0);
+
+  // Generate HTML for each forecast card
+  const forecastHTML = forecastArr
+    .map((forecast) => `
+      <div class="col-sm-2 mb-3">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">${dayjs.unix(forecast.dt).format("MM/DD/YYYY")}</h5>
+            <img src="https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" alt="${forecast.weather[0].description}">
+            <p class="temp">Temp: ${forecast.main.temp} °F</p>
+            <p class="wind">Wind: ${forecast.wind.speed} MPH</p>
+            <p class="humidity">Humidity: ${forecast.main.humidity}%</p>
+          </div>
+        </div>
+      </div>
+    `).join("");
+
+  // Update the forecast row with the generated HTML
+  forecastRow.innerHTML = forecastHTML;
+}
+
+// Function to update the search history
+function updateSearchHistory(cityName) {
+  // Create a new list item with a button for the city name
+  const li = document.createElement("li");
+  li.classList.add("list-group-item");
+  li.innerHTML = `<button type="button" class="btn btn-secondary w-100">${cityName}</button>`;
+
+  // Append the new list item to the search history
+  searchHistory.appendChild(li);
+}
